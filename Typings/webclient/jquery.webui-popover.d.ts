0b4c7d584a8d658d19a78c618ad9122837dcb809
Elkhov Denis <Elkhov.D@docsvision.com>
2017-05-31 15:11:19 +0300

    remove unused d ts

--- Typings/webclient/jquery.webui-popover.d.ts
+++
@@ -1,7 +0,0 @@
-
-
-declare function createWebUIPopover(elem: HTMLElement, options: any): void;
-
-declare function showPopover(elem: HTMLElement): void;
-
-declare function hidePopover(elem: HTMLElement): void;
